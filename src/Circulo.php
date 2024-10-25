abe2e2c6c680cff45a333f2bc3ed47bc0816cf9e
Ana <gutierrez.baron.ana@iescamas.es>
2024-10-25 10:11:46 +0200

    Añadimos el constructor

--- src/Circulo.php
+++ src/Circulo.php
@@ -3,5 +3,10 @@ include_once 'Figura.php';
 class Circulo extends FiguraGeometrica{
     private Punto $centro;
     private float $radio;
+
+    public function __construct($centro, $radio) {
+        $this->centro = $centro;
+        $this->radio = $radio;
+    }
 }
 ?>
